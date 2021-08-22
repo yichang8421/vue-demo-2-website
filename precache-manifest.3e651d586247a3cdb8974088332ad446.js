df9a8695179958449005c61d0d060157b838a40e
andy8421 <yichang8421@qq.com>
2021-08-22 21:02:47 +0800

    deploy

---
+++ precache-manifest.3e651d586247a3cdb8974088332ad446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148099d329ebf4bcb975",
+    "url": "/vue-demo-2-website/css/app.cd7164fd.css"
+  },
+  {
+    "revision": "9b52fcae4cfc5a0a1f4c6ec889354433",
+    "url": "/vue-demo-2-website/index.html"
+  },
+  {
+    "revision": "148099d329ebf4bcb975",
+    "url": "/vue-demo-2-website/js/app.53a48639.js"
+  },
+  {
+    "revision": "f41e1ff6ac16240f5e18",
+    "url": "/vue-demo-2-website/js/chunk-vendors.b3d9c18c.js"
+  },
+  {
+    "revision": "1b150cec0b06e8d6c4d1babc86ff96e5",
+    "url": "/vue-demo-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-demo-2-website/robots.txt"
+  }
+]);
